a6ddafe66bc32327b86489a2314bed2bf50adfdd
Jesdakorn Samittiauttakorn <i@ton.in.th>
2013-02-18 22:58:06 +0700

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaBlender::Application.config.secret_token = 'ecaeb58985d6d9fb4ca08f52d9de0273f373b9c7a2bbc9f42efcd2376b4c5351b36aff7156508f7898ca7e6638b24088f41b7c066c4b15f564b466d5aaf68a3c'
