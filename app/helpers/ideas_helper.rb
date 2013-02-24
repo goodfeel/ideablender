fa24600e9585e6a582d2fc1f1567859acc3cd0ee
Jesdakorn S <ton@oozou.com>
2013-02-24 13:47:15 +0700

    update label color and div size

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,14 @@
 module IdeasHelper
+  def label_color(category)
+    if category.length > 10
+      ""
+    elsif category.length > 8
+      "label-warning"
+    elsif category.length > 7
+      "label-success"
+    elsif category.length > 6
+      "label-important"
+    else
+      "label-info"
+  end
 end
